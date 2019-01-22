1408dc3c77191408b2ce9b11a8fc5e294091ab10
Rayleigh <gmj1197@gmail.com>
2019-01-23 01:59:54 +0900

    feat: Add deepspeech lambda function

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+const AWS = require('aws-sdk');
 const dsb = require('./dsBuffer');
 
+const s3 = new AWS.S3();
+
+function onError(err) {
+  console.log(`error: ${err}`);
+  return 1;
+}
+
+function readFile(bucketName, filename) {
+  const params = { Bucket: bucketName, Key: filename };
+
+  s3.getObject(params, (err, data) => {
+    if (!err) onError(err);
+
+    console.log(data.Body.toString());
+    return data.Body.toString();
+  });
+}
+
 exports.handler = async event => {
-  // TODO implement
+  const outputGraph = readFile('models', event.s3.models.outputGraph);
+  const alphabet = readFile('models', event.s3.models.alphabet);
+  const lm = readFile('models', event.s3.models.lm);
+  const trie = readFile('models', event.s3.models.trie);
+
   const response = {
     statusCode: 200,
-    body: dsb(event.buffer),
+    body: dsb(event.buffer, outputGraph, alphabet, lm, trie),
   };
 
-  if (response.statusCode !== 200) return 0;
+  if (response.statusCode !== 200) return 1;
   return response.body;
 };
